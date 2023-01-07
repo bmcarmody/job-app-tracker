277b3aededed9e95ae70a9aa066648df7ec4c144
Brandon Carmody <BmcPrimary@yahoo.com>
2023-01-07 14:06:10 -0700

    Add input to ApplicationForm

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -1,22 +1,12 @@
+import { Input } from '../../library/Input';
+
 export const ApplicationForm = () => {
   return (
     <div className="bg-slate-600 p-4 rounded w-full flex flex-col text-slate-200">
-      <label>
-        Company Name:
-        <input type="text" className="ml-4 mb-4 text-black px-4 py-1 rounded outline-none" />
-      </label>
-      <label>
-        Position:
-        <input type="text" className="ml-4 mb-4 text-black px-4 py-1 rounded outline-none" />
-      </label>
-      <label>
-        Date Applied:
-        <input type="date" className="ml-4 mb-4 text-black px-4 py-1 rounded outline-none" />
-      </label>
-      <label>
-        Job Posting Link:
-        <input type="text" className="ml-4 text-black px-4 py-1 rounded outline-none" />
-      </label>
+      <Input label="Company Name" />
+      <Input label="Position" />
+      <Input label="Date Applied" type="date" />
+      <Input label="Job Posting Link" />
     </div>
   );
 };
