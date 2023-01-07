e12f52e43cd527fc53e478205ea6a106d260bbf2
Brandon Carmody <BmcPrimary@yahoo.com>
2023-01-07 12:04:23 -0700

    Configure prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-  return (
-    <div className="text-3xl font-bold underline">
-      Test
-    </div>
-  )
+  return <div className="text-3xl font-bold underline">test</div>;
 };
 
-export default App
+export default App;
