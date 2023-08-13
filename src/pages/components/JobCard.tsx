d0859e51ec31806e9769691c248f931a6563ae7e
Brandon Carmody <brandon.carmody@maestroqa.com>
2023-08-13 17:30:31 -0600

    Add confidence level and icons

--- src/pages/components/JobCard.tsx
+++ src/pages/components/JobCard.tsx
@@ -1,28 +1,34 @@
 import { type ReactElement } from "react";
-import * as Tooltip from "@radix-ui/react-tooltip";
-import { RiMoneyDollarCircleLine, RiTimeLine } from "react-icons/ri";
+import { RiArrowUpLine, RiBuilding4Line, RiExternalLinkFill, RiMoneyDollarCircleLine, RiTimeLine } from "react-icons/ri";
+import { Tooltip } from "./Tooltip";
 
 export const JobCard = (): ReactElement<HTMLElement> => {
   return (
     <section className="bg-slate-800 rounded-lg p-3 outline outline-offset-2 outline-1 outline-slate-600 cursor-grab text-slate-100">
       <h3 className="font-display text-lg font-medium">Google</h3>
-      <p className="font-body text-xs">Senior Frontend Engineer</p>
-      <p className="font-body text-base">Remote</p>
-      <footer className="flex items-center leading-5 text-xs justify-between">
+      <p className="font-body text-xs flex items-center hover:text-rose-600 hover:cursor-pointer">Senior Frontend Engineer <RiExternalLinkFill className="ml-0.5 text-sm" /></p>
+      <section className="font-body text-xs flex items-center mt-1 justify-between">
         <div className="flex items-center">
-          <RiMoneyDollarCircleLine className="text-xl mr-0.5 text-green-400" />
+          <RiBuilding4Line className="text-l mr-0.5" />
+          Remote
+        </div>
+        <div className="ml-auto">
+          <Tooltip tooltipContent="High Confidence Level">
+            <RiArrowUpLine className="text-green-400 text-xl" />
+          </Tooltip>
+        </div>
+      </section>
+      <footer className="flex items-center leading-5 text-xs justify-between mt-1">
+        <div className="flex items-center">
+          <RiMoneyDollarCircleLine className="text-xl mr-0.5" />
           100k - 120k
         </div>
-        <Tooltip.Provider delayDuration={0}>
-          <Tooltip.Root>
-            <Tooltip.Trigger className="flex items-center ml-auto">
-              <><RiTimeLine className="text-xl mr-0.5" /> 5 days ago</>
-            </Tooltip.Trigger>
-            <Tooltip.Content className="p-2 bg-purple-600 rounded-lg">
-              Applied 01/01/2023
-            </Tooltip.Content>
-          </Tooltip.Root>
-        </Tooltip.Provider>
+        <Tooltip
+          triggerClassName="flex items-center ml-auto"
+          tooltipContent={<>Applied 01/01/2023</>}
+        >
+          <RiTimeLine className="text-xl mr-0.5" /> 5 days ago
+        </Tooltip>
       </footer>
     </section>
   );
