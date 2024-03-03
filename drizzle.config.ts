e98df620d617e35df7d4fb2bf75bae6f3bf04b07
Brandon Carmody <BmcPrimary@yahoo.com>
2024-03-03 10:14:53 -0800

    Replace npm with bun

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/schema/*",
+  schema: "./src/schemas/*",
   driver: "mysql2",
   dbCredentials: {
     uri: process.env["DATABASE_URL"] as string,
