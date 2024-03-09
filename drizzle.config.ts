209e5b2c56e161fc9c9317ab7e9d38840e3d393c
Brandon Carmody <BmcPrimary@yahoo.com>
2024-03-08 20:20:43 -0800

    Migrate to Postgres / Supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./src/schemas/*",
-  driver: "mysql2",
+  driver: "pg",
   dbCredentials: {
-    uri: process.env["DATABASE_URL"] as string,
+    connectionString: process.env["DATABASE_URL"] as string,
   },
   out: "./migrations",
 } satisfies Config;
--- src/schemas/application.ts
+++ src/schemas/application.ts
@@ -1,13 +1,13 @@
 import {
   varchar,
-  int,
-  mysqlTable,
+  integer,
+  pgTable,
   index,
   timestamp,
-} from "drizzle-orm/mysql-core";
+} from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
 
-export const application = mysqlTable(
+export const application = pgTable(
   "application",
   {
     id: varchar("id", { length: 128 })
@@ -15,17 +15,17 @@ export const application = mysqlTable(
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
       .$defaultFn(() => createId()),
     createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(), // Postgres does not have an equivalent of onUpdateNow()
     userId: varchar("user_id", { length: 30 }).notNull(),
     statusId: varchar("status_id", { length: 30 }).notNull(),
     position: varchar("position", { length: 30 }).notNull(),
     companyName: varchar("company_name", { length: 30 }).notNull(),
     jobPostUrl: varchar("job_post_url", { length: 500 }).notNull(),
-    confidenceLevel: int("confidence_level").notNull(),
-    applyDate: timestamp("apply_date").notNull(),
+    confidenceLevel: integer("confidence_level").notNull(),
+    applyDate: timestamp("apply_date").notNull(), // Postgres does not have an equivalent of defaultNow() for timestamps
   },
-  (Application) => ({
-    userIdIdx: index("user_id_idx").on(Application.userId),
-    statusIdIdx: index("status_id_idx").on(Application.statusId),
+  (application) => ({
+    userIdIdx: index("user_id_idx").on(application.userId),
+    statusIdIdx: index("status_id_idx").on(application.statusId),
   })
 );
