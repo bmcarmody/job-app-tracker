5c1aa07a65e47eb97bd1ff8051108b4d8e75293d
Brandon Carmody <BmcPrimary@yahoo.com>
2024-03-08 20:03:22 -0800

    Remove clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,8 @@
 "use client";
 
-import { useAuth, useUser } from "@clerk/nextjs";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
-import Link from "next/link";
 
 const Navbar = () => {
-  const { isSignedIn } = useUser();
-  const { isLoaded, signOut } = useAuth();
-
-  const getAuthLink = () => {
-    if (!isLoaded) return null;
-
-    if (isSignedIn) {
-      return (
-        <button
-          className="rounded-md bg-rose-600 px-4 py-2 font-medium"
-          onClick={() => void signOut()}
-        >
-          Sign Out
-        </button>
-      );
-    }
-
-    return (
-      <Link href="/sign-in" className="text-slate-100">
-        <button
-          className="rounded-md bg-rose-600 px-4 py-2 font-medium"
-          onClick={() => void signOut()}
-        >
-          Sign In
-        </button>
-      </Link>
-    );
-  };
 
   return (
     <NavigationMenu.Root className="mb-8 flex list-none items-center justify-between text-slate-300">
@@ -41,7 +11,6 @@ const Navbar = () => {
       </NavigationMenu.Item>
       <div>
         <NavigationMenu.NavigationMenuItem>
-          {getAuthLink()}
         </NavigationMenu.NavigationMenuItem>
       </div>
     </NavigationMenu.Root>
