0d53f43ddd4b37ea7adc8a183eb25e785495210a
Brandon Carmody <BmcPrimary@yahoo.com>
2024-03-30 10:18:11 -0700

    Add dropdown menu to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import { isAuthed, logout } from '~/app/(auth)/login/actions';
 import { AddJobModal } from './dashboard/AddJobModal';
+import {
+  ArrowLeftStartOnRectangleIcon,
+  UserIcon,
+} from '@heroicons/react/24/solid';
 
 const Navbar = async () => {
   const isLoggedIn = await isAuthed();
@@ -11,14 +15,35 @@ const Navbar = async () => {
       {isLoggedIn ? (
         <div>
           <AddJobModal />
-          <form>
-            <button
-              className="btn btn-outline btn-secondary"
-              formAction={logout}
+          <div className="dropdown dropdown-end">
+            <div className="avatar placeholder" role="button" tabIndex={0}>
+              <div className="w-12 cursor-pointer rounded-full bg-neutral text-neutral-content hover:opacity-90">
+                <span>BC</span>
+              </div>
+            </div>
+            <ul
+              className="menu dropdown-content z-[1] mt-2 w-52 rounded-box bg-base-200 p-2 shadow"
+              tabIndex={0}
             >
-              Logout
-            </button>
-          </form>
+              <li>
+                <a>
+                  <UserIcon className="size-4" /> My profile
+                </a>
+              </li>
+              <li>
+                <form>
+                  <button
+                    className="flex items-center gap-2"
+                    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                    formAction={logout}
+                  >
+                    <ArrowLeftStartOnRectangleIcon className="size-4" />
+                    Logout
+                  </button>
+                </form>
+              </li>
+            </ul>
+          </div>
         </div>
       ) : (
         <Link className="btn btn-primary" href="login">
